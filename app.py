5adfeb8c825098988b5fa3e79e656c483acfb031
Dashmeet Singh <dashmeetsinghkkr@gmail.com>
2026-02-10 23:33:33 +0530

    Fixed port binding

--- app.py
+++ app.py
@@ -246,4 +246,5 @@ def add_contact_us():
 
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
